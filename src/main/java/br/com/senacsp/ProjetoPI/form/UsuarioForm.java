5d0d59a33dde9d01748e177adb8420bc54ea445d
Luciano <luc.merce@hotmail.com>
2023-03-12 19:42:19 -0300

    Criando enum's para Autorizacao e Status / Criando imutabilidade para o email / Criando validação para evitar duplicidade de email / Alteração das requisições para os critérios alterados.

--- src/main/java/br/com/senacsp/ProjetoPI/form/UsuarioForm.java
+++ src/main/java/br/com/senacsp/ProjetoPI/form/UsuarioForm.java
@@ -1,5 +1,7 @@
 package br.com.senacsp.ProjetoPI.form;
 
+import br.com.senacsp.ProjetoPI.grupos.Status;
+import br.com.senacsp.ProjetoPI.grupos.Autorizacao;
 import br.com.senacsp.ProjetoPI.model.Usuario;
 
 public class UsuarioForm {
@@ -9,13 +11,17 @@ public class UsuarioForm {
     private String cpf;
     private String usuario;
     private String senha;
+    private Status status;
+    private Autorizacao autorizacao;
 
-    public UsuarioForm(Long id, String nome, String cpf, String usuario, String senha) {
+    public UsuarioForm(Long id, String nome, String cpf, String usuario, String senha, Status status, Autorizacao autorizacao) {
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.usuario = usuario;
         this.senha = senha;
+        this.status = status;
+        this.autorizacao = autorizacao;
     }
 
     public Usuario conversor(UsuarioForm dto) {
@@ -24,7 +30,9 @@ public class UsuarioForm {
                 dto.getNome(),
                 dto.getCpf(),
                 dto.getUsuario(),
-                dto.getSenha()
+                dto.getSenha(),
+                dto.getAtivo(),
+                dto.getAutorizacao()
         );
         return usuario;
     }
@@ -67,6 +75,22 @@ public class UsuarioForm {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Status getAtivo() {
+        return status;
+    }
+
+    public void setAtivo(Status status) {
+        this.status = status;
+    }
+
+    public Autorizacao getAutorizacao() {
+        return autorizacao;
+    }
+
+    public void setAutorizacao(Autorizacao autorizacao) {
+        this.autorizacao = autorizacao;
     }
 }
 
